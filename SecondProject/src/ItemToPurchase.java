20b34ae255aa3a31f4244cec3380db802d3fc2ee
MauriceBurger <maurice@merantix.com>
2020-10-15 14:40:44 -0400

    Added default constructor and getters and setters

--- SecondProject/src/ItemToPurchase.java
+++ SecondProject/src/ItemToPurchase.java
@@ -1,11 +1,38 @@
-
 public class ItemToPurchase {
 
     String itemName;
     int itemPrice;
     int itemQuantity;
     
+    ItemToPurchase() {
+	
+    }
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    
     
 }
  
